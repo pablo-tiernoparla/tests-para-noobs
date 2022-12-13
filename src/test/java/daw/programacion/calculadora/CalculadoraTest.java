84917d6dd024b42c50b498ede19114afd55807cd
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-12-13 21:30:33 +0100

    primer test

--- src/test/java/daw/programacion/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -1,9 +1,16 @@
 package daw.programacion.calculadora;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculadoraTest {
     
-    
+    @Test
+    public void dosSumarDosEsCuatro(){
+        int expected = 4;
+        int num1 = 2, num2 = 2;
+        Calculadora calc = new Calculadora();
+
+        assertEquals(expected, calc.sumar(num1, num2));
+    }
 }
