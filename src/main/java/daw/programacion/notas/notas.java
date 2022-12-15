7959709d8486740528bfdd84c1e041fcdd9f9b7d
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-12-15 17:40:29 +0100

    notas

--- src/main/java/daw/programacion/notas/notas.java
+++ src/main/java/daw/programacion/notas/notas.java
@@ -2,4 +2,23 @@ package daw.programacion.notas;
 
 public class notas {
     
+    public String notaTexto(double nota){
+        String notaEnTexto;
+
+        if(nota < 0 || nota > 10){
+            notaEnTexto = "invalido";
+        } else if (nota < 5) {
+            notaEnTexto = "suspenso";
+        } else if (nota < 7) {
+            notaEnTexto = "aprobado";
+        } else if (nota < 9) {
+            notaEnTexto = "notable";
+        } else if (nota < 10) {
+            notaEnTexto = "sobresaliente";
+        } else {
+            notaEnTexto = "matricula";
+        }
+
+        return notaEnTexto;
+    }
 }
