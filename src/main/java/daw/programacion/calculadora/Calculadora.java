c4891430641f87669f7aa58e8bde0ec3e8a9b35c
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-12-15 16:49:45 +0100

    restar y multiplicar

--- src/main/java/daw/programacion/calculadora/Calculadora.java
+++ src/main/java/daw/programacion/calculadora/Calculadora.java
@@ -5,4 +5,12 @@ public class Calculadora {
     public int sumar(int num1, int num2){
         return num1 + num2;
     }
+
+    public int restar(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public int multiplicar(int num1, int num2){
+        return num1 * num2;
+    }
 }
