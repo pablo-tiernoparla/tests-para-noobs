d13994fafe5169bb03b02330e812e6efcb0d717a
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-12-16 20:59:48 +0100

    tests notastests

--- src/test/java/daw/programacion/notasTest/notasTest.java
+++ src/test/java/daw/programacion/notasTest/notasTest.java
@@ -15,4 +15,52 @@ public class notasTest {
         notas notas = new notas();
         assertEquals(expected, notas.notaTexto(notaSacada));
     }
+
+    @Test
+    public void notaSuperiorDiez(){
+        String expected = "invalido";
+        double notaSacada = 11.6;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
+
+    @Test
+    public void notaSuspensa(){
+        String expected = "suspenso";
+        double notaSacada = 2.5;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
+
+    @Test
+    public void notaAprobada(){
+        String expected = "aprobado";
+        double notaSacada = 5.4;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
+
+    @Test
+    public void notaNotable(){
+        String expected = "notable";
+        double notaSacada = 8.8;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
+
+    @Test
+    public void notaSobresaliente(){
+        String expected = "sobresaliente";
+        double notaSacada = 9.7;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
+
+    @Test
+    public void notaMatricula(){
+        String expected = "matricula";
+        double notaSacada = 10;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
 }
