7d15cd314e1d950916534c79b07fff594b4ad812
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2022-12-16 20:55:17 +0100

    test notastest

--- src/test/java/daw/programacion/notasTest/notasTest.java
+++ src/test/java/daw/programacion/notasTest/notasTest.java
@@ -1,5 +1,18 @@
 package daw.programacion.notasTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import daw.programacion.notas.notas;
+
 public class notasTest {
     
+    @Test
+    public void notaNegativaError(){
+        String expected = "invalido";
+        double notaSacada = -5.4;
+        notas notas = new notas();
+        assertEquals(expected, notas.notaTexto(notaSacada));
+    }
 }
